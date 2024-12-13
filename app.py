1975ffa2daf0dcc1484c249ab7df75b19a94a7de
Jonathan Stern <jstern718@gmail.com>
2024-12-13 00:20:13 -0500

    bug

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 
 from flask import Flask, render_template, request, flash, redirect, session, g
-from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 from forms import UserAddForm, UserEditForm, LoginForm, MessageForm, CSRFProtectForm
@@ -16,9 +15,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-toolbar = DebugToolbarExtension(app)
 
 
 try:
